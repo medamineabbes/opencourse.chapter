c1fba4c80cf2670836c19fd8fb7f2b28725fb95e
medamineabbes <medamineabbes2@gmail.com>
2022-08-10 00:19:40 +0100

    Fix

--- src/main/java/com/opencourse/chapter/repos/ChapterRepo.java
+++ src/main/java/com/opencourse/chapter/repos/ChapterRepo.java
@@ -2,6 +2,8 @@ package com.opencourse.chapter.repos;
 
 import com.opencourse.chapter.entities.Chapter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
 public interface ChapterRepo extends JpaRepository<Chapter,Long>{
     
 }
--- src/main/java/com/opencourse/chapter/repos/ElementRepo.java
+++ src/main/java/com/opencourse/chapter/repos/ElementRepo.java
@@ -1,6 +1,10 @@
 package com.opencourse.chapter.repos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.opencourse.chapter.entities.Element;
+
+@Repository
 public interface ElementRepo extends JpaRepository<Element,Long>{
     
 }
