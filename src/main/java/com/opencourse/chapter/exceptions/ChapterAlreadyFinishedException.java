39c1ce15293a6e5e80b107d97d7242d8d205b72e
medamineabbes <medamineabbes2@gmail.com>
2022-08-11 16:42:47 +0100

    bind exceptions to status codes

--- src/main/java/com/opencourse/chapter/exceptions/ChapterAlreadyFinishedException.java
+++ src/main/java/com/opencourse/chapter/exceptions/ChapterAlreadyFinishedException.java
@@ -1,5 +1,9 @@
 package com.opencourse.chapter.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class ChapterAlreadyFinishedException extends RuntimeException{
     public ChapterAlreadyFinishedException(Long id){
         super("chapter with id : " + id + " already finished");
--- src/main/java/com/opencourse/chapter/exceptions/ChapterAlreadyUnFinishedException.java
+++ src/main/java/com/opencourse/chapter/exceptions/ChapterAlreadyUnFinishedException.java
@@ -1,5 +1,9 @@
 package com.opencourse.chapter.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class ChapterAlreadyUnFinishedException extends RuntimeException{
     public ChapterAlreadyUnFinishedException(Long id){
         super("chapter with id : " + id + " already not finished");
