3ca9b0faf82dd658935eccb374c79fa8bd42b808
medamineabbes <medamineabbes2@gmail.com>
2022-08-11 18:22:11 +0100

    api and api documetation swagger

---
+++ src/main/java/com/opencourse/chapter/api/ChapterController.java
@@ -0,0 +1,68 @@
+package com.opencourse.chapter.api;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.opencourse.chapter.dtos.ChapterDto;
+import com.opencourse.chapter.dtos.ChapterLimitedDto;
+import com.opencourse.chapter.services.ChapterService;
+
+import lombok.AllArgsConstructor;
+//https://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
+@RestController
+@RequestMapping("/api/v1/chapter")
+@AllArgsConstructor
+public class ChapterController {
+    private final ChapterService service;
+
+    @PostMapping
+    public ResponseEntity<ChapterLimitedDto> addChapter(@RequestBody(required=true) @Valid ChapterDto chapter){
+        return ResponseEntity.ok(service.addChapter(chapter));
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<ChapterDto> getById(@PathVariable(required=true) Long id){
+        return ResponseEntity.ok(service.getChapterById(id));
+    }
+    
+    @GetMapping("/section/{sectionId}")
+    public ResponseEntity<List<ChapterLimitedDto>> getChaptersBySectionId(@PathVariable(name = "sectionId") Long sectionId){
+        return ResponseEntity.ok(service.getChaptersBySectionId(sectionId));
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateChapter(@RequestBody(required=true) @Valid ChapterLimitedDto chapter){
+        service.updateChapter(chapter);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteChapter(@PathVariable(required=true) Long chapterId){
+        service.deleteCahpterById(chapterId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/finish/{id}")
+    public ResponseEntity<Void> finishChapter(@PathVariable(required=true) Long chapterId){
+        Long userId=1L;//get from spring security later
+        service.markChapterAsFinished(userId, chapterId);
+        return ResponseEntity.ok().build();
+    }
+    @PostMapping("/unfinish/{id}")
+    public ResponseEntity<Void> unfinishChapter(@PathVariable(required=true) Long chapterId){
+        Long userId=1L;
+        service.markChapterAsUnFinished(userId, chapterId);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/opencourse/chapter/api/ElementController.java
@@ -0,0 +1,41 @@
+package com.opencourse.chapter.api;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.opencourse.chapter.dtos.ElementDto;
+import com.opencourse.chapter.services.ChapterService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/element")
+@AllArgsConstructor
+public class ElementController {
+    private final ChapterService service;
+
+    @PostMapping
+    public ResponseEntity<ElementDto> addElement(@RequestBody @Valid ElementDto element){
+        return ResponseEntity.ok(service.addElement(element));
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateElement(@RequestBody @Valid ElementDto element){
+        service.updateElement(element);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteElement(@PathVariable Long id){
+        service.deleteElementById(id);
+        return ResponseEntity.ok().build();
+    }
+}
