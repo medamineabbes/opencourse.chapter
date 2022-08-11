a6b298d58d0cbc35a7f6dddf4157dbf49cd08904
medamineabbes <medamineabbes2@gmail.com>
2022-08-11 17:05:09 +0100

    validation for ChapterLimitedDto

--- src/main/java/com/opencourse/chapter/dtos/ChapterLimitedDto.java
+++ src/main/java/com/opencourse/chapter/dtos/ChapterLimitedDto.java
@@ -2,12 +2,22 @@ package com.opencourse.chapter.dtos;
 import com.opencourse.chapter.entities.Chapter;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class ChapterLimitedDto {
     private Long id;
+    @NotBlank(message="title is mandatory")
+    @Size(max=50,message="max number of characters is 50")
     private String title;
+    @NotBlank(message="video is mandatory")
     private String videoUrl;
+    @NotBlank(message="description is mandatory")
+    @Size(max=300,message="max number of characters is 300")
     private String description;
+    @NotNull
     private Long sectionId;
     public static ChapterLimitedDto fromChapter(Chapter c){
         ChapterLimitedDto cd=new ChapterLimitedDto();
