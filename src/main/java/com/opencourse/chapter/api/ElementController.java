e75613acb3d4f1cd2258e4f049677959a50fc356
medamineabbes <medamineabbes2@gmail.com>
2022-09-18 14:59:41 +0100

    api comm & course service

--- src/main/java/com/opencourse/chapter/api/ElementController.java
+++ src/main/java/com/opencourse/chapter/api/ElementController.java
@@ -21,19 +21,24 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/api/v1/element")
 @AllArgsConstructor
 public class ElementController {
+    
     private final ChapterService service;
+
+    //only authentic users
     @GetMapping
     public ResponseEntity<ElementDto> getElemenetById(Long id){
         Long userId=15L;
         return ResponseEntity.ok(service.getElementById(id,userId));
     }
 
+    //only teachers
     @PostMapping
     public ResponseEntity<Long> addElement(@RequestBody @Valid ElementDto element){
         Long userId=15L;
         return ResponseEntity.ok(service.addElement(element,userId));
     }
 
+    //only teachers
     @PutMapping
     public ResponseEntity<Void> updateElement(@RequestBody @Valid ElementDto element){
         Long userId=15L;
@@ -41,6 +46,7 @@ public class ElementController {
         return ResponseEntity.ok().build();
     }
 
+    //only teachers
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteElement(@PathVariable Long id){
         Long userId=15L;
@@ -48,6 +54,7 @@ public class ElementController {
         return ResponseEntity.ok().build();
     }
 
+    //authentic users
     @GetMapping("/chapter/{id}")
     public ResponseEntity<List<ElementDto>> getElementsByChapterId(@PathVariable(required=true) Long id){
         Long userId=15L;
--- src/main/java/com/opencourse/chapter/externalservices/CourseService.java
+++ src/main/java/com/opencourse/chapter/externalservices/CourseService.java
@@ -1,16 +1,40 @@
 package com.opencourse.chapter.externalservices;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.Data;
 
 @Service
 public class CourseService{
 
+    RestTemplate restTemplate;
+    private String baseUrl="https://opencourse-course.herokuapp.com/api/v1";
+    public CourseService(){
+        restTemplate=new RestTemplate();
+    }
+
     public boolean userHasAccessToSection(Long sectionId,Long userId){
-        //call course service rest template
-        return false;
+        AccessDto dto=new AccessDto();
+        dto.setSectionId(sectionId);
+        dto.setUserId(userId);
+        ResponseEntity<Boolean> response=restTemplate.postForEntity(baseUrl + "/subscription", dto, Boolean.class);
+        return response.getBody();
     }
+    
     public boolean userCreatedSection(Long sectionId,Long userId){
-        //call course service rest template
-        return false;
+        AccessDto dto=new AccessDto();
+        dto.setSectionId(sectionId);
+        dto.setUserId(userId);
+        ResponseEntity<Boolean> response=restTemplate.postForEntity(baseUrl + "/course/section/creator", dto, Boolean.class);
+        return response.getBody();
     }
+
+}
+
+@Data
+class AccessDto{
+    private Long userId;
+    private Long sectionId; 
 }
