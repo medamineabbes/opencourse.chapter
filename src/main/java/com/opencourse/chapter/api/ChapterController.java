87dc8c2b87098372240c8c6be4291a2c12ac9b39
medamineabbes <medamineabbes2@gmail.com>
2022-09-21 17:45:13 +0100

    get userId from security context

--- src/main/java/com/opencourse/chapter/api/ChapterController.java
+++ src/main/java/com/opencourse/chapter/api/ChapterController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,28 +33,28 @@ public class ChapterController {
     //only teachers
     @PostMapping
     public ResponseEntity<Long> addChapter(@RequestBody(required=true) @Valid ChapterDto chapter){
-        Long userId=15L;
+        Long userId=Long.valueOf(SecurityContextHolder.getContext().getAuthentication().getName());
         return ResponseEntity.ok(service.addChapter(chapter,userId));
     }
     
     //authentic users
     @GetMapping("/{id}")
     public ResponseEntity<ChapterDto> getById(@PathVariable(required=true) Long id){
-        Long userId=15L;
+        Long userId=Long.valueOf(SecurityContextHolder.getContext().getAuthentication().getName());
         return ResponseEntity.ok(service.getChapterById(id,userId));
     }
     
     //authentic users
     @GetMapping("/section/{sectionId}")
     public ResponseEntity<List<ChapterDto>> getChaptersBySectionId(@PathVariable(name = "sectionId") Long sectionId){
-        Long userId=15L;
+        Long userId=Long.valueOf(SecurityContextHolder.getContext().getAuthentication().getName());
         return ResponseEntity.ok(service.getChaptersBySectionId(sectionId,userId));
     }
 
     //only teachers
     @PutMapping
     public ResponseEntity<Object> updateChapter(@RequestBody(required=true) @Valid ChapterDto chapter){
-        Long userId=15L;
+        Long userId=Long.valueOf(SecurityContextHolder.getContext().getAuthentication().getName());
         service.updateChapter(chapter,userId);
         return ResponseEntity.ok().build();
     }
@@ -61,7 +62,7 @@ public class ChapterController {
     //only teacher
     @DeleteMapping("/{chapterId}")
     public ResponseEntity<Object> deleteChapter(@PathVariable(required=true) Long chapterId){
-        Long userId=15L;
+        Long userId=Long.valueOf(SecurityContextHolder.getContext().getAuthentication().getName());
         service.deleteChapterById(chapterId,userId);
         return ResponseEntity.ok().build();
     }
@@ -69,7 +70,7 @@ public class ChapterController {
     //authentic users
     @GetMapping("/finish/{chapterId}")
     public ResponseEntity<Object> finishChapter(@PathVariable(required=true) Long chapterId){
-        Long userId=15L;
+        Long userId=Long.valueOf(SecurityContextHolder.getContext().getAuthentication().getName());
         service.markChapterAsFinished(chapterId,userId);
         return ResponseEntity.ok().build();
     }
@@ -77,7 +78,7 @@ public class ChapterController {
     //authentic users
     @GetMapping("/unfinish/{chapterId}")
     public ResponseEntity<Object> unfinishChapter(@PathVariable(required=true) Long chapterId){
-        Long userId=1L;
+        Long userId=Long.valueOf(SecurityContextHolder.getContext().getAuthentication().getName());
         service.markChapterAsUnFinished(chapterId,userId);
         return ResponseEntity.ok().build();
     }
--- src/main/java/com/opencourse/chapter/api/ElementController.java
+++ src/main/java/com/opencourse/chapter/api/ElementController.java
@@ -3,6 +3,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,21 +28,21 @@ public class ElementController {
     //only authentic users
     @GetMapping("/{elementId}")
     public ResponseEntity<ElementDto> getElemenetById(@PathVariable Long elementId){
-        Long userId=15L;
+        Long userId=Long.valueOf(SecurityContextHolder.getContext().getAuthentication().getName());
         return ResponseEntity.ok(service.getElementById(elementId,userId));
     }
 
     //only teachers
     @PostMapping
     public ResponseEntity<Long> addElement(@RequestBody @Valid ElementDto element){
-        Long userId=15L;
+        Long userId=Long.valueOf(SecurityContextHolder.getContext().getAuthentication().getName());
         return ResponseEntity.ok(service.addElement(element,userId));
     }
 
     //only teachers
     @PutMapping
     public ResponseEntity<Object> updateElement(@RequestBody @Valid ElementDto element){
-        Long userId=15L;
+        Long userId=Long.valueOf(SecurityContextHolder.getContext().getAuthentication().getName());
         service.updateElement(element,userId);
         return ResponseEntity.ok().build();
     }
@@ -49,7 +50,7 @@ public class ElementController {
     //only teachers
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteElement(@PathVariable Long id){
-        Long userId=15L;
+        Long userId=Long.valueOf(SecurityContextHolder.getContext().getAuthentication().getName());
         service.deleteElementById(id,userId);
         return ResponseEntity.ok().build();
     }
@@ -57,7 +58,7 @@ public class ElementController {
     //authentic users
     @GetMapping("/chapter/{id}")
     public ResponseEntity<List<ElementDto>> getElementsByChapterId(@PathVariable(required=true) Long id){
-        Long userId=15L;
+        Long userId=Long.valueOf(SecurityContextHolder.getContext().getAuthentication().getName());
         return ResponseEntity.ok(service.getElementsByChapterId(id,userId));
     }
 
